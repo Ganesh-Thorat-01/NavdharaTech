b8fcef63a31b5b6aa005e3924040d8fe7f6afb17
Ganesh-Thorat-01 <thorat.ganeshscoe@gmail.com>
2020-11-22 12:43:51 +0530

    url file

--- NavdharaTech/urls.py
+++ NavdharaTech/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path("",include('blog.urls')),
     path('members/',include('django.contrib.auth.urls')),
     path("members/",include('members.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
