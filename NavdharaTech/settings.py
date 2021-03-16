c70976413d09c10b8ef61ee61a90643ef6d34037
Ganesh Thorat <73458337+Ganesh-Thorat-01@users.noreply.github.com>
2021-03-16 23:33:26 +0530

    Update settings.py

--- NavdharaTech/settings.py
+++ NavdharaTech/settings.py
@@ -138,7 +138,7 @@ MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS =(
     os.path.join(BASE_DIR,'static'),
 )
-STATIC_ROOT=BASE_DIR / 'staticfiles' 
+STATIC_ROOT=os.path.join(BASE_DIR, ‘static’) 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 '''#HTTP setting
